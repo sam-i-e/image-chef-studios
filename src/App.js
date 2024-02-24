9ab84083e28f17a0e63d9b8cd78cdaa47a3ab3f9
codelord-evans <ekioko404@gmail.com>
2024-02-24 13:52:27 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Image Chef studios
+      </header>
+    </div>
+  );
+}
+
+export default App;
