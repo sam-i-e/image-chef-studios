5d7217ded35be130f5b0a71cbd1f65811aa79f5a
sam-i-e <samierapo@outlook.com>
2024-03-05 12:57:21 +0300

    Homepage Initialization

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../styles/Homepage.css';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import logoOfficial from "../Assets/logoOfficial.png"
-import cart from "../Assets/cart.png"
+// import cart from "../Assets/cart.png"
 import homepage from "../Assets/homepage.jpg"
 
 
@@ -11,40 +11,20 @@ import homepage from "../Assets/homepage.jpg"
 function Homepage() {
   return (
     <div className="homepage">
+      <img
+            className="logo1"
+            loading="eager"
+            alt="logo"
+            src={logoOfficial}
+          />
       <h1>Welcome to Image Chef Studios</h1>
-      <p>Sonnie Muthoni</p>
-    {/* <div class="header">
-    <div class="container">
-        <div class="navbar">
-            <div class="logo">
-                <Link to=""><img src={logoOfficial.png} alt="logo" width="350px"></img></Link>
-            </div>
-            <nav>
-               <ul>
-                <li><a href="/Homepage">Home</a></li>
-                <li><a href="/Gallery">Gallery</a></li>
-                <li><a href="/Bookings">Bookings</a></li>
-                <li><a href="/About">About us</a></li>
-                <li><a href="/About">Contact us</a></li>
-               </ul>
-            </nav>
-            <Link to=""><img src={cart.png} alt="cart" width="30px" height="30px"></img></Link>
-        </div>
-        <div class="row">
-            <div class="col-2">
-                <h1>Let's capture your memories!</h1>
-                <p>Do you ever stop and think about how bad you're going to miss a moment you're living long after it's gone? Worry no more.
-                    <br></br>We capture memories so you can re-live them over and over again!</p>
-                    <div class="landing">
-                        <a href="#sliding" class="btn">Dive right in &#8594;</a>
-                    </div>
-            </div>
-            <div class="col-2">
-               <Link to=""><img src={homepage.jpg} alt="homepage" width="600px"></img> </Link>
-            </div>
-        </div>
-    </div>
-    </div> */}
+      <p>Welcome to The Image Chef Studios, where every moment is a masterpiece waiting to be captured. Our passion for photography goes beyond the lens – it's about storytelling, creativity, and preserving memories that last a lifetime. Whether you're celebrating a milestone, capturing the essence of your brand, or simply cherishing the beauty of everyday life, our team of skilled photographers is here to transform your vision into stunning imagery. From weddings to corporate events, family portraits to professional headshots, we specialize in crafting captivating visuals that evoke emotion and inspire wonder. Step into our world of creativity, where each frame tells a unique story. Let us be your guides on this visual journey, and together, let's create moments that truly shine.</p>
+      <img
+            className="homeimage1"
+            loading="eager"
+            alt="homeimage"
+            src={homepage}
+          />
     </div>
     
   );
