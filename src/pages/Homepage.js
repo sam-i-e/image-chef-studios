fe1461257a978a39b68151e1331a8790f2b2793d
sam-i-e <samierapo@outlook.com>
2024-03-26 22:23:52 +0300

    Home Initialization

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,9 +6,11 @@ import logoOfficial from "../Assets/logoOfficial.png"
 // import cart from "../Assets/cart.png"
 // import homepage from "../Assets/homepage.jpg"
 // import homepage2 from "../Assets/homepage2.jpg"
-import homepage5 from "../Assets/homepage5.jpg"
-import homepage6 from "../Assets/homepage6.jpg"
+import homepage11 from "../Assets/homepage11.jpg"
+// import homepage6 from "../Assets/homepage6.jpg"
 import Image from "../Assets/DJI_0896_2.jpg"
+import homepage12 from "../Assets/homepage12.png"
+import homepage13 from "../Assets/homepage13.png"
 
 
 
@@ -16,14 +18,17 @@ import Image from "../Assets/DJI_0896_2.jpg"
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="hero" style={{backgroundImage: `url(${Image})`}}>
-      <img
+       <img
             className="logo1"
             loading="eager"
             alt="logo"
             src={logoOfficial}
           />
-      <h1>The<br></br> Image Chef Studios</h1>
+      <div className="hero" style={{backgroundImage: `url(${Image})`}}>
+      <div className='hero-cont'>
+      {/* <h1>The<br></br> Image Chef Studios</h1> */}
+      {/* <h2>Do you ever stop and think about how bad you're going to miss a moment you're living long after it's gone?<br></br> Worry no more. */}
+{/* We capture memories so you can re-live them over and over again!</h2> */}
       <h2>Where every snapshot tells a story <br></br>and every frame<br></br> captures a moment of pure magic.</h2>
       {/* <img
             className="homeimage6"
@@ -31,17 +36,31 @@ function Homepage() {
             alt="homeimage"
             src={homepage6}
           /> */}
-      <h3>Welcome to The Image Chef Studios,<br></br> where every moment is a masterpiece waiting to be captured.<br></br> Our passion for photography goes beyond the lens – it's about storytelling, creativity and preserving memories that last a lifetime.</h3>
+      {/* <h3>Welcome to The Image Chef Studios,<br></br> where every moment is a masterpiece waiting to be captured.<br></br> Our passion for photography goes beyond the lens – it's about storytelling, creativity and preserving memories that last a lifetime.</h3> */}
       </div>
+      </div>
+      <div className='illustration-container'>
+      <img
+            className="homepage12"
+            loading="eager"
+            alt="logo"
+            src={homepage12}
+          />
+        <div className='card'>
+          <h1>Journey Through the Lens</h1>
+          <p>We capture the essence of life's most precious moments. From weddings to family portraits, corporate events to breathtaking landscapes,</p>
+        </div>
+
+        </div>
       <section className="prompt">
         <div className="card">
-          <h1>Our passion for photography transcends mere snapshots; it's about crafting timeless stories through the lens.</h1>
+          <h1>Our passion for photography goes beyond mere snapshots; it's about crafting timeless stories through the lens.</h1>
           <p>Whether you're celebrating a milestone, capturing the essence of your brand, or simply cherishing the beauty of everyday life,<br></br> our team of skilled photographers is here to transform your vision into stunning imagery </p>
           <img
-            className="homeimage5"
+            className="homeimage11"
             loading="eager"
             alt="homeimage"
-            src={homepage5}
+            src={homepage11}
           />
           <div className="get-started-button1">
           <button>Book With Us</button>
@@ -50,15 +69,30 @@ function Homepage() {
       </Button> */}
           {/* <a href="/saver" className="saver-schedule-demo-button"> Book With us</a> */}
               </div>
+              
           <div className="navigate">
             <div className="get-started-button1">
              </div>
+             
               
             {/* <a href="/saver" className="saver-schedule-demo-button"> Plan a future</a> */}
           </div>
         </div>
         
       </section>
+      <div className='illustration-container2'>
+      <img
+            className="homepage13"
+            loading="eager"
+            alt="logo"
+            src={homepage13}
+          />
+        <div className='card'>
+          <h1>See the World Through our Eyes</h1>
+          <p>Explore our portfolio and let us transform your moments into works of art that you'll cherish for a lifetime.</p>
+        </div>
+
+        </div>
     </div>
     
   );
