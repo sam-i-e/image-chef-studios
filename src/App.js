8d8742a3f80608526f1f684bb89dbf62e158e010
sam-i-e <samierapo@outlook.com>
2024-03-30 09:24:43 +0300

    Home Initialization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
-
-
-
+// import { carousel } from './Components/carousel';
+// import carousel from '../Components/carousel';
 
 function App() {
   return (
+    // <div className='App'>
+    //   <carousel />
+    // </div>
     <Router> 
       <Routes>
         <Route path="/" element={<Homepage/>} />
--- src/Components/carousel.js
+++ src/Components/carousel.js
@@ -1,4 +1,5 @@
 // import React from 'react'
+import '../styles/carousel.css'
 
 function carousel() {
   return (
