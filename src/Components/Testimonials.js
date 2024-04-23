bf404b5714636dda9648738cf3ebe3821ce6aeeb
sam-i-e <samierapo@outlook.com>
2024-04-23 12:05:34 +0300

    Testimonials

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -5,12 +5,12 @@ const Testimonials = () => {
   const testimonialsData = [
     {
       id: 1,
-      name: 'John Doe',
+      name: 'Mike Kimani',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     },
     {
       id: 2,
-      name: 'Jane Doe',
+      name: 'Faith Wanjiru',
       content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     },
   ];
